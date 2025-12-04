d25e5bc5500e031148786d5a5008c499ed5bb748
BALUMEDAMONI <balumedamoni123@gmail.com>
2025-12-04 16:16:08 +0000

    first commit

---
+++ secondcode.py
@@ -0,0 +1,20 @@
+x = 5
+y = 7
+z = x * y
+print("Multiplication:", z)
+a = [2, 4, 6, 8, 10]
+b = []
+for i in a:
+    b.append(i + 1)
+print("List after adding 1:", b)
+name = "Balu"
+age = 20
+print("Name:", name)
+print("Age:", age)
+sum_numbers = 0
+for n in range(1, 11):
+    sum_numbers += n
+print("Sum of 1 to 10:", sum_numbers)
+even = [n for n in range(20) if n % 2 == 0]
+print("Even numbers:", even)
+
